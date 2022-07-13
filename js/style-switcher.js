67e32317ec5e465642475db48145ed8a0ad179dd
Mahi purohit <mahipurohit174@gmail.com>
2022-07-13 22:02:00 +0530

    done

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,24 +1,24 @@
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click",()=>{
-    document.querySelector(".style-switcher").classList.toggle("open");
-}
-)
-window.addEventListener("scroll",()=>{
-    if(document.querySelector(".style-switcher").classList.contains("open")){
-        document.querySelector(".style-switcher").classList.remove("open");
-    }
-})
-const alternateStyle = document.querySelectorAll(".alternate-style");
-function setActiveStyle (color){
-    alternateStyle.forEach((style) =>{
-        if(color === style.getAttribute("title")){
-            style.removeAttribute("disabled");
-        }
-        else{
-            style.setAttribute("disabled" , "true ")
-        }
-    }
-)}
+// const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+// styleSwitcherToggle.addEventListener("click",()=>{
+//     document.querySelector(".style-switcher").classList.toggle("open");
+// }
+// )
+// window.addEventListener("scroll",()=>{
+//     if(document.querySelector(".style-switcher").classList.contains("open")){
+//         document.querySelector(".style-switcher").classList.remove("open");
+//     }
+// })
+// const alternateStyle = document.querySelectorAll(".alternate-style");
+// function setActiveStyle (color){
+//     alternateStyle.forEach((style) =>{
+//         if(color === style.getAttribute("title")){
+//             style.removeAttribute("disabled");
+//         }
+//         else{
+//             style.setAttribute("disabled" , "true ")
+//         }
+//     }
+// )}
 // day night
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click",()=>{
